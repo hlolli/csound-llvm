885e9f0e54222a36a2b51f406784e000aee5e93f
hlolli <hlolli@gmail.com>
2019-04-18 16:52:28 +0200

    fix missing file and rename

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nix-build -E 'with import <nixpkgs> {}; callPackage ./env.nix {}' --show-trace && \
+nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}' --show-trace && \
     rm -rf ./lib && mkdir lib && cp -rf result/lib/csound.bc ./lib && chmod -R +rw ./lib
